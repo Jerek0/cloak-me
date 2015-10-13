66951125c1ffdf4948f793bd42c284c6892332a7
unknown <jminie@LIC-02.GOBELINS-ANNECY.local>
2015-10-13 11:38:01 +0200

    Added firebase support

--- app/src/main/java/com/jeremy_minie/helloagaincrm/home/MainActivity.java
+++ app/src/main/java/com/jeremy_minie/helloagaincrm/home/MainActivity.java
@@ -11,11 +11,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.jeremy_minie.helloagaincrm.R;
-import com.jeremy_minie.helloagaincrm.home.LoginFragment;
-import com.jeremy_minie.helloagaincrm.home.RegisterFragment;
 import com.jeremy_minie.helloagaincrm.user.UserActivity;
-import com.jeremy_minie.helloagaincrm.user.User;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements LoginFragment.LoginListener, RegisterFragment.RegisterListener {
 
@@ -24,10 +26,15 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
     public static final String PASSWORD = TAG+".password";
     public static final String MAIL = TAG+".mail";
 
+    private Firebase fbRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+        fbRef = new Firebase("https://helloagaincrm.firebaseio.com/");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("HelloAgainCrm");
@@ -78,13 +85,35 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
     }
 
     @Override
-    public void onLoginClicked(CharSequence username, CharSequence password) {
+    public void onLoginClicked(final CharSequence username, final CharSequence password) {
+        final MainActivity scope = this;
+
         Log.d(TAG, "onClickedLoginBtn");
         if(username.length() > 0 && password.length() > 0) {
-            Intent intent = new Intent(this, UserActivity.class);
-            intent.putExtra(USERNAME, username);
-            intent.putExtra(PASSWORD, password);
-            startActivity(intent);
+            fbRef.authWithPassword(username.toString(), password.toString(), new Firebase.AuthResultHandler() {
+                @Override
+                public void onAuthenticated(AuthData authData) {
+                    System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+
+                    Intent intent = new Intent(scope, UserActivity.class);
+                    intent.putExtra(USERNAME, username);
+                    intent.putExtra(MAIL, username);
+                    intent.putExtra(PASSWORD, password);
+                    startActivity(intent);
+                }
+                @Override
+                public void onAuthenticationError(FirebaseError firebaseError) {
+                    Log.e(TAG,firebaseError.toString());
+                    Snackbar.make(findViewById(R.id.mainContainer), firebaseError.getMessage(), Snackbar.LENGTH_SHORT)
+                            .setAction("DISMISS", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Log.d(TAG, "Dismiss");
+                                }
+                            })
+                            .show();
+                }
+            });
         } else {
             Snackbar.make(findViewById(R.id.mainContainer), "Username and password don't match !", Snackbar.LENGTH_SHORT)
                     .setAction("DISMISS", new View.OnClickListener() {
@@ -101,11 +130,37 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
     public void onRegisterClicked(CharSequence username, CharSequence mail, CharSequence password) {
         Log.d(TAG, "onClickedRegisteerBtn " + username + " " + password);
         if(username.length() > 0 && password.length() > 0 && mail.length() > 0) {
-            Intent intent = new Intent(this, UserActivity.class);
-            intent.putExtra(USERNAME, username);
-            intent.putExtra(MAIL, mail);
-            intent.putExtra(PASSWORD, password);
-            startActivity(intent);
+            fbRef.createUser(mail.toString(), password.toString(), new Firebase.ValueResultHandler<Map<String, Object>>() {
+                @Override
+                public void onSuccess(Map<String, Object> result) {
+                    System.out.println("Successfully created user account with uid: " + result.get("uid"));
+
+                    LoginFragment fragment = new LoginFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainContainer, fragment).commit();
+
+                    Snackbar.make(findViewById(R.id.mainContainer), "You are now registered, please login", Snackbar.LENGTH_SHORT)
+                            .setAction("DISMISS", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Log.d(TAG, "Dismiss");
+                                }
+                            })
+                            .show();
+                }
+                @Override
+                public void onError(FirebaseError firebaseError) {
+                    // there was an error
+                    Log.e(TAG,firebaseError.toString());
+                    Snackbar.make(findViewById(R.id.mainContainer), firebaseError.getMessage(), Snackbar.LENGTH_SHORT)
+                            .setAction("DISMISS", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Log.d(TAG, "Dismiss");
+                                }
+                            })
+                            .show();
+                }
+            });
         } else {
             Snackbar.make(findViewById(R.id.mainContainer), "You have to fill all inputs", Snackbar.LENGTH_SHORT)
                     .setAction("DISMISS", new View.OnClickListener() {
