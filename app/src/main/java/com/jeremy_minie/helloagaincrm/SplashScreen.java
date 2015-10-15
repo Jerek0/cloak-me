ff8cbb255399ddc4124d551c8c4448ab029b592a
Jerek0 <jeremy.minie@gmail.com>
2015-10-15 09:03:35 +0200

    Added splashscreen + background-image on login

---
+++ app/src/main/java/com/jeremy_minie/helloagaincrm/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.jeremy_minie.helloagaincrm;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    private static final long SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
